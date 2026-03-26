cb33a89dcec8c3ffd958425663f76b9d0b6b081b
gurman05 <gurmandeepk20@gmail.com>
2026-03-26 21:00:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//  YAHAN apna MongoDB URL daalna
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
